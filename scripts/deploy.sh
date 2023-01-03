5a5fb36cbbf78bafe6bc6ec3a1a2f721e209839e
komekami <komekami0131@gmail.com>
2023-01-03 23:01:10 +0900

    feat: prismaのインストール

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-gcloud functions deploy \
-  insertFanartTweets \
-  --region=asia-northeast1 \
-  --runtime=nodejs16 \
-  --source=. \
-  --entry-point=insertFanartTweets \
-  --trigger-http
