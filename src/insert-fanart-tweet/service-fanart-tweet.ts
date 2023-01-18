8c8b6a65bd8a225a78ef6ab2494410a3170723d5
komekami <komekami0131@gmail.com>
2023-01-19 08:59:27 +0900

    refactor: バッチのファイルを分割

--- src/insert-fanart-tweet/service-fanart-tweet.ts
+++ src/insert-fanart-tweet/service-fanart-tweet.ts
@@ -1,69 +1,45 @@
-import * as dotenv from "dotenv"
-import { Logger } from "tslog"
+import { hashtags, tweet_objects, media_objects } from '@prisma/client';
 import {
   TwitterApi,
   TwitterApiReadOnly,
   TweetSearchRecentV2Paginator,
 } from "twitter-api-v2"
-import { PrismaClient } from '@prisma/client'
 
-import { CreateTweetData } from '../interface/create-tweet-data.interface'
-import { CreateMediaData } from '../interface/create-media-data.interface'
-import { ILogObj } from "tslog/dist/types/interfaces"
+import { DaoFanartTweet } from "./dao-fanart-tweet"
 
-export class Logic {
-  private logger: Logger<ILogObj>
-  private prisma: PrismaClient
+export class ServiceFanartTweet {
   private roClient: TwitterApiReadOnly
+  private dao: DaoFanartTweet
 
   constructor() {
-    this.logger = new Logger({ name: "insertFanartTweets" })
-    if (process.env.PRODUCTION_ENV !== "prod") {
-      dotenv.config()
-    }
     const BEARER_TOKEN = process.env.BEARER_TOKEN ?? ""
-
-    this.prisma = new PrismaClient()
     const client = new TwitterApi(BEARER_TOKEN)
     this.roClient = client.readOnly
+
+    this.dao = new DaoFanartTweet()
   }
 
   /**
    * @remarks
-   * ツイートを取得し、DBにインサートするバッチ処理
+   * hashtagsテーブルのカラムをすべて取得
+   *
+   * @returns hashtags[]
    */
-  public async doExecute() {
-    this.logger.info("Start")
-    try {
-      const hashtagList = await this.prisma.hashtags.findMany()
-
-      for await (const hashtag of hashtagList) {
-        this.logger.debug(`Hashtag: ${hashtag['tagName']}`)
-        const fanartTweets = await this.fetchTweets(hashtag['tagName'])
-
-        await this.insertTweets(hashtag['id'], fanartTweets)
-        await this.insertMedia(fanartTweets)
-      }
-    } catch (e) {
-      this.logger.fatal(e)
-      this.logger.info("Failed")
-      process.exit(1)
-    }
-    this.logger.info("Success")
-    process.exit(0)
+  public async getHashtags(): Promise<hashtags[]> {
+    return await this.dao.getHashtags()
   }
 
   /**
    * @remarks
    * TwitterAPIを用いて前日分のツイートを取得する処理
    *
-   * @param hashtag - 取得したいツイートに付与されているハッシュタグ名
+   * @param hashtagName - 取得したいツイートに付与されているハッシュタグ名
    * @returns 取得したツイート配列
    */
-  private async fetchTweets(
-    hashtag: string,
+  public async fetchTweets(
+    hashtagName: string,
   ): Promise<TweetSearchRecentV2Paginator> {
-    const searchKeyword = `#${hashtag} -is:retweet has:images`
+    const searchKeyword = `#${hashtagName} -is:retweet has:images`
     const [yesterdayMidnight, todayMidnight] = await this.setWithinTime()
 
     const fanartTweets = await this.roClient.v2.search(searchKeyword, {
@@ -101,21 +77,21 @@ export class Logic {
 
   /**
    * @remarks
-   * tweetをDBにinsertする処理
+   * 取得したツイートを整形しDBにinsertする処理
    *
-   * @param id - Hashtagの番号
+   * @param id - hashtagsテーブルのID
    * @param fanartTweets - twitter-api-v2で取得したツイート配列
    * @returns void
    */
-  private async insertTweets(
+  public async insertTweets(
     id: number,
     fanartTweets: TweetSearchRecentV2Paginator,
   ): Promise<void> {
-    const data: CreateTweetData[] = []
+    const data: tweet_objects[] = []
     for await (const fanartTweet of fanartTweets) {
       const [text, tweetUrl] = await this.extractTweetUrl(fanartTweet['text'])
 
-      const tweetData: CreateTweetData = {
+      const tweetData: tweet_objects = {
         tweetId: fanartTweet['id'],
         text: text,
         retweetCount: Number(fanartTweet['public_metrics']['retweet_count']),
@@ -123,39 +99,14 @@ export class Logic {
         authorId: fanartTweet['author_id'],
         tweetUrl: tweetUrl,
         tweetedAt: new Date(fanartTweet['created_at']),
+        createdAt: new Date(),
+        updatedAt: new Date(),
         hashtagId: id,
       }
       data.push(tweetData)
     }
-    await this.prisma.tweet_objects.createMany({ data, skipDuplicates: true })
-  }
 
-  /**
-   * @remarks
-   * mediaをDBにinsertする処理
-   *
-   * @param fanartTweets - twitter-api-v2で取得したツイート配列
-   * @returns void
-   */
-  private async insertMedia(
-    fanartTweets: TweetSearchRecentV2Paginator
-  ): Promise<void> {
-    const data: CreateMediaData[] = []
-    for await (const fanartTweet of fanartTweets) {
-      // media.fieldsは追加情報
-      const mediaFields = fanartTweets.includes.medias(fanartTweet)
-      // 画像は複数枚ある
-      for await (const mediaField of mediaFields) {
-        const mediaData: CreateMediaData = {
-          type: mediaField['type'],
-          // 画像以外のmedia_typeも考慮
-          url: mediaField['url'] ?? mediaField['variants'][0]['url'],
-          tweetId: fanartTweet['id'],
-        }
-        data.push(mediaData)
-      }
-    }
-    await this.prisma.media_objects.createMany({ data, skipDuplicates: true })
+    await this.dao.insertTweetObjects(data)
   }
 
   /**
@@ -171,5 +122,36 @@ export class Logic {
     const text = tweetText.replace(` ${tweetUrl}`, '')
 
     return [text, tweetUrl]
+  }
+
+  /**
+   * @remarks
+   * 取得したツイートのメディアを整形しDBにinsertする処理
+   *
+   * @param fanartTweets - twitter-api-v2で取得したツイート配列
+   * @returns void
+   */
+  public async insertMedia(
+    fanartTweets: TweetSearchRecentV2Paginator
+  ): Promise<void> {
+    const data: media_objects[] = []
+    for await (const fanartTweet of fanartTweets) {
+      // media.fieldsは追加情報
+      const mediaFields = fanartTweets.includes.medias(fanartTweet)
+      // 画像は複数枚ある
+      for await (const mediaField of mediaFields) {
+        const mediaData: media_objects = {
+          type: mediaField['type'],
+          // 画像以外のmedia_typeも考慮
+          url: mediaField['url'] ?? mediaField['variants'][0]['url'],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          tweetId: fanartTweet['id'],
+        }
+        data.push(mediaData)
+      }
+    }
+
+    await this.dao.insertMediaObjects(data)
   }
 }
