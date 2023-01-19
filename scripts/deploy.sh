e04a7f555ec1d98fe932cc533d4580cb5c00ada2
komekami <komekami0131@gmail.com>
2023-01-19 09:12:13 +0900

    update: build/deployスクリプトの更新

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# Tips: 開発環境のPlanetScaleはローカルでconnectしておかないと"Can't reach server"エラーになる
+rm -rf dist
+
+# docker-compose up でnode_modulesの名前付きボリュームを作っておく
+docker exec -it \
+  victo-batch \
+  sh -c "npm run build"
+
 gcloud functions deploy \
   insertFanartTweets \
   --region=asia-northeast1 \
